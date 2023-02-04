0b6a6ae44f721feac8d1c3108c02c3090abe99a5
anita <anubhui27@gmail.com>
2023-02-04 22:37:03 +0530

    add hibernate Project

--- DemoCollection/src/com/velocity/test/ArrayListDemo.java
+++ DemoCollection/src/com/velocity/test/ArrayListDemo.java
@@ -1,9 +1,17 @@
 package com.velocity.test;
 
+import java.util.ArrayList;
+
 public class ArrayListDemo {
 
 	public static void main(String[] args) {
-		System.out.println("hello");
-
+		ArrayList<Integer>al=new ArrayList<Integer>();
+		al.add(10);
+		al.add(20);
+		al.add(30);
+	
+		for(int i:al) {
+			System.out.println(al);
+		}
 	}
 }
